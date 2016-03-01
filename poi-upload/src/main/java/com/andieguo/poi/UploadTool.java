6a8c714c674ab8ca55838a5991b808ed5540d8c3
andieguo <andieguo@foxmail.com>
2016-03-01 13:32:34 +0800

    实现upload测试用例

--- poi-upload/src/main/java/com/andieguo/poi/UploadTool.java
+++ poi-upload/src/main/java/com/andieguo/poi/UploadTool.java
@@ -3,8 +3,11 @@ package com.andieguo.poi;
 import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileStatus;
@@ -14,6 +17,16 @@ import org.apache.hadoop.io.IOUtils;
 import org.apache.hadoop.util.Progressable;
 import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.andieguo.poi.dao.CityDao;
+import com.andieguo.poi.dao.POIDao;
+import com.andieguo.poi.pojo.POI;
+import com.andieguo.poi.util.Constants;
+import com.andieguo.poi.util.ExcelWrite;
+import com.andieguo.poi.util.Record;
 
 public class UploadTool {
 	
@@ -87,24 +100,50 @@ public class UploadTool {
 		return true;
 	}
 	
+	/* excel column formate:column_#_width, excel中每一列的名称 */
+	public static final String[] RECORES_COLUMNS = new String[] { "timedifference_#_5000", "poiNumber_#_5000","fileNumber_#_5000" };
+	/* the column will display on xls files. must the same as the entity fields.对应上面的字段. */
+	public static final String[] RECORES_FIELDS = new String[] { "timedifference", "poiNumber","fileNumber" };
+	
 	public static void main(String[] args) throws Exception {
 		UploadTool uploadTool = new UploadTool();
-//		if (args.length < 2) {
-//			//logger.info("Please input two number");
-//			System.exit(2);
-//		}
-//		String localSrc = args[0];
-//		String dst = args[1];
-		String localSrc = "C:\\Users\\andieguo\\poi-data\\北京";
-		String dst = "/user/hadoop/poi-data/北京";
+		@SuppressWarnings("resource")
+		ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		CityDao cityDao = (CityDao) context.getBean("cityDao");
+		POIDao poiDao = (POIDao) context.getBean("poiDao");
+		List<String> cityList = cityDao.findAll();
+		List<POI> poiList = poiDao.findByType(0);
+		List<Record> records = new ArrayList<Record>();
 		Configuration conf = new Configuration();
 		conf.set("fs.default.name", "hdfs://master:9000");//很关键
 		FileSystem fs = FileSystem.get(conf);
-		File srcFile = new File(localSrc);
-		if (srcFile.isDirectory()) {
-			uploadTool.copyDirectory(localSrc, dst, fs);
-		} else {
-			uploadTool.copyFile(localSrc, dst, fs);
+		long starttime = System.currentTimeMillis();
+		for(String city : cityList){
+			for(POI poi : poiList){
+				String localSrc = Constants.DATAPATH+File.separator+city+File.separator+poi.getPoivalue();
+				String dst = "/user/hadoop/poi-data/"+city+"/"+poi.getPoivalue();
+				File srcFile = new File(localSrc);
+				if(srcFile.exists()){
+					if (srcFile.isDirectory()) {
+						uploadTool.copyDirectory(localSrc, dst, fs);
+					} else if(srcFile.isFile()) {
+						uploadTool.copyFile(localSrc, dst, fs);
+					}
+				}
+				break;
+			}
+			long endtime = System.currentTimeMillis();
+			Record record = new Record(endtime-starttime,0,0);
+			records.add(record);
+			break;
 		}
+		if(fs != null) fs.close();
+		HSSFWorkbook workbook = new HSSFWorkbook();
+		ExcelWrite<Record> userSheet = new ExcelWrite<Record>();
+		userSheet.creatAuditSheet(workbook, "上传数据记录", records, RECORES_COLUMNS, RECORES_FIELDS);
+
+		FileOutputStream fileOut = new FileOutputStream(new File(Constants.MKDIRPATH + File.separator + "loadrecord.xls"));
+		workbook.write(fileOut);
+		fileOut.close();
 	}
 }
